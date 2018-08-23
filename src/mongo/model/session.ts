45b70b88a3292d1f795a0108f0503e9cc4d2cccb
lnhorwood <github@lukehorwood.co.uk>
2018-08-23 15:57:15 +0100

    Replaced hardcoded secret with usage of uuid generater that generates a new secret with each start-up - to be replaced with certificate

--- src/mongo/model/session.ts
+++ src/mongo/model/session.ts
@@ -1,10 +1,12 @@
 import { sign, verify, VerifyCallback } from 'jsonwebtoken';
 import { bindNodeCallback, Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import hat from 'hat';
 import { User } from '../schema/user';
 import { TimeInSeconds } from './time-in-seconds';
 
 export class Session {
+  private static readonly _secret: string = hat();
   private readonly _exp: number;
 
   constructor(private _user: User, expiresIn: TimeInSeconds = TimeInSeconds.HOUR) {
@@ -19,13 +21,13 @@ export class Session {
   }
 
   get token(): string {
-    return sign(this.toJSON(), 'potatoes');
+    return sign(this.toJSON(), Session._secret);
   }
 
   static validate(token: string): Observable<string> {
     return bindNodeCallback((token: string, secret: string, callback: VerifyCallback) =>
       verify(token, secret, callback)
-    )(token, 'potatoes').pipe(
+    )(token, Session._secret).pipe(
       catchError(() => throwError('Invalid Token.')),
       map((session: { user: User }) => new Session(session.user).token)
     );
